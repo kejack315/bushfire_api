f895d2ef46746240d8c2484d6f25ad5b3354293e
zke <zke920407@gmail.com>
2024-04-17 10:21:04 +0800

    update userlocation controller

--- API/app/Http/Controllers/UserLocationController.php
+++ API/app/Http/Controllers/UserLocationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Suburb;
 use App\Models\UserLocation;
 use App\Http\Requests\StoreUserLocationRequest;
 use App\Http\Requests\UpdateUserLocationRequest;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserLocationController extends Controller
 {
@@ -43,12 +48,14 @@ class UserLocationController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(UserLocation $userLocation)
+    public function edit()
     {
-        $user = auth()->user();
-
+        $user = Auth::user();
         if ($user) {
-            return response()->json(['user' => $user], 200);
+            $userSuburbID = $user->primary_location_suburb_id;
+            $userSuburbName = Suburb::where('id', '=', $userSuburbID)->value('name');
+
+            return response()->json(['user' => $user, 'suburb_name' => $userSuburbName], 200);
         } else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
@@ -57,16 +64,22 @@ class UserLocationController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserLocationRequest $request, UserLocation $userLocation)
+    public function update(Request $request)
     {
-        $user = auth()->user();
-
+        $user = Auth::user();
 
         if ($user) {
-            $user->suburb = $request->input('suburb');
+            $request->validate([
+                'suburb' => 'required|exists:suburbs,name',
+            ]);
+
+            $suburbName = $request->input('suburb');
+            $suburb = Suburb::where('name', $suburbName)->first();
+
+            $user->primary_location_suburb_id = $suburb->id;
             $user->save();
 
-            return response()->json(['message' => 'User location updated successfully'], 200);
+            return response()->json(['message' => 'Primary suburb updated successfully', 'suburb_name' => $suburbName], 200);
         } else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
